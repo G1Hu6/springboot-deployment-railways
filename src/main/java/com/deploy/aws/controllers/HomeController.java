a287d2a16bbb9e38f1b7e71ac6c5c17f6be00650
G1Hu6 <r1ruru219@gmail.com>
2025-01-19 23:27:47 +0530

    Home page controller is added

---
+++ src/main/java/com/deploy/aws/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.deploy.aws.controllers;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class HomeController{
+
+    @GetMapping
+    public ResponseEntity<String> home(){
+        return ResponseEntity.ok("Home Page is Running Successfully");
+    }
+}
+
