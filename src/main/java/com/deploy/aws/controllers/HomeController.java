9a1b561a215f731b56920d2a7789607818f1e4bd
G1Hu6 <r1ruru219@gmail.com>
2025-01-20 13:14:40 +0530

    using env for port

--- src/main/java/com/deploy/aws/controllers/HomeController.java
+++ src/main/java/com/deploy/aws/controllers/HomeController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping
 public class HomeController{
 
-    @GetMapping
+    @GetMapping(path = "/")
     public ResponseEntity<String> home(){
         return ResponseEntity.ok("Home Page is Running Successfully");
     }
